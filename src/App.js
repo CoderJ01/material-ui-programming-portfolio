20dafb1a27eda81a8efcb618ec8eb48b2442b01c
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-01 17:39:25 -0800

    use React Router to route to pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout.component";
+import AboutMe from "./pages/AboutMe/AboutMe.page";
+import Portfolio from "./pages/Portfolio/Portfolio.page";
+import Skills from "./pages/Skills/Skills.page";
+import ContactMe from "./pages/ContactMe/ContactMe.page";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<AboutMe/>}/>
+          <Route path="portfolio" element={<Portfolio/>}/>
+          <Route path="skills" element={<Skills/>}/>
+          <Route path="contact-me" element={<ContactMe/>}/>
+          <Route path="*" element={<AboutMe/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
