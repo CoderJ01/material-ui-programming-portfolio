e19f80d9278b5d1479733820222cd92eb5f4f91a
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-01 19:52:59 -0800

    change import

--- src/components/Layout/Layout.component.jsx
+++ src/components/Layout/Layout.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom'; 
-import styles from './Layout.style.css';
+import './Layout.style.css';
 
 const Layout = () => {
     return (
