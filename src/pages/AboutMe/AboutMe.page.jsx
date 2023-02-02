81907f26039242de21e586c147c72a179d4b2eed
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-02 15:07:08 -0800

    add Avatar from MUI

--- src/pages/AboutMe/AboutMe.page.jsx
+++ src/pages/AboutMe/AboutMe.page.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
 
 const AboutMe = () => {
     return (
         <React.Fragment>
             <CssBaseline />
             <Container maxWidth="md">
-            <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+                <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+                <Avatar
+                    alt="Remy Sharp"
+                    src="/static/images/avatar/1.jpg"
+                    sx={{ width: 56, height: 56 }}
+                />
+                </Box>
             </Container>
         </React.Fragment>
     );
