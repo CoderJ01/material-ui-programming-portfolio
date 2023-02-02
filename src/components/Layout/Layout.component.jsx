9faa6e85ff840b4c6a64e700996f8aa4cf33016f
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-01 19:51:50 -0800

    import and implement styles

--- src/components/Layout/Layout.component.jsx
+++ src/components/Layout/Layout.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom'; 
+import styles from './Layout.style.css';
 
 const Layout = () => {
     return (
         <div>
-            <nav>
-                <ul>
-                    <li>
+            <nav className="layoutnav">
+                <ul className="layoutul">
+                    <li className="layoutli">
                         <Link to="/">About Me</Link>
                     </li>
                     <li>
