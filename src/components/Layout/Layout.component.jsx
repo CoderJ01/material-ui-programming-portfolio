6e1aa4404fbaae8ff4bbffc658084bb98953ead3
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-01 17:45:16 -0800

    add links to Layout component

--- src/components/Layout/Layout.component.jsx
+++ src/components/Layout/Layout.component.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Outlet, Link } from 'react-router-dom'; 
 
 const Layout = () => {
     return (
         <div>
-
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">About Me</Link>
+                </li>
+                <li>
+                    <Link to="/portfolio">Portfolio</Link>
+                </li>
+                <li>
+                    <Link to="/skills">Skills</Link>
+                </li>
+                <li>
+                    <Link to="/contact-me">Contact Me</Link>
+                </li>
+            </ul>
+        </nav>
+        <Outlet />
         </div>
     );
 }
