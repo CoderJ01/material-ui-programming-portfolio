fcc40f7ab85c0d0617be3c8f99a2f1c416b8a563
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-01 18:07:03 -0800

    import DrawerAppBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout/Layout.component";
+// import Layout from "./components/Layout/Layout.component";
+import DrawerAppBar from "./components/Header/Header.component";
 import AboutMe from "./pages/AboutMe/AboutMe.page";
 import Portfolio from "./pages/Portfolio/Portfolio.page";
 import Skills from "./pages/Skills/Skills.page";
@@ -9,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout/>}>
+        <Route path="/" element={<DrawerAppBar/>}>
           <Route index element={<AboutMe/>}/>
           <Route path="portfolio" element={<Portfolio/>}/>
           <Route path="skills" element={<Skills/>}/>
