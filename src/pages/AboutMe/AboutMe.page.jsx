9a907aba9131cfe2a40457b8d2a6b853cbed1207
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-02 15:43:17 -0800

    add description

--- src/pages/AboutMe/AboutMe.page.jsx
+++ src/pages/AboutMe/AboutMe.page.jsx
@@ -5,6 +5,7 @@ import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import image from './images/01_avatar.png'
 import { Typography } from '@mui/material';
+import { paragraphs } from './AboutMe.util';
 
 const AboutMe = () => {
     return (
@@ -22,36 +23,13 @@ const AboutMe = () => {
                 </div>
                 <Container maxWidth="sm" sx={{marginTop: '5%'}}>
                     <Box sx={{ bgcolor: 'blue', height: '50vh' }}>
-                    <Typography variant="body1" gutterBottom>
-                        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-                        blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
-                        neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
-                        quasi quidem quibusdam.
-                    </Typography>
-                    <Typography variant="body1" gutterBottom>
-                        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-                        blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
-                        neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
-                        quasi quidem quibusdam.
-                    </Typography>
-                    <Typography variant="body1" gutterBottom>
-                        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-                        blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
-                        neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
-                        quasi quidem quibusdam.
-                    </Typography>
-                    <Typography variant="body1" gutterBottom>
-                        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-                        blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
-                        neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
-                        quasi quidem quibusdam.
-                    </Typography>
-                    <Typography variant="body1" gutterBottom>
-                        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-                        blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
-                        neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
-                        quasi quidem quibusdam.
-                    </Typography>
+                        {
+                            paragraphs.map(paragraph => {
+                                return (
+                                    <Typography variant="body1" gutterBottom sx={{ color: 'black' }}>{paragraph}</Typography>
+                                );
+                            })
+                        }
                     </Box>
                 </Container>
                 </Box>
