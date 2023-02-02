2d05102d28371b475c4c3ebbfa46b0cab662393c
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-02 15:03:08 -0800

    import and utilize MUI Container component

--- src/pages/AboutMe/AboutMe.page.jsx
+++ src/pages/AboutMe/AboutMe.page.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 const AboutMe = () => {
     return (
-        <div>
-            About Me
-        </div>
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="sm">
+            <Box sx={{ bgcolor: '#cfe8fc', minheight: '100vh' }} />
+            </Container>
+        </React.Fragment>
     );
 }
 
