08257b5a07be3ffca9261a00ea55a9780e96970e
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-02 15:54:00 -0800

    add header to portfolio

--- src/pages/Portfolio/Portfolio.page.jsx
+++ src/pages/Portfolio/Portfolio.page.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import { Typography } from '@mui/material';
 
 const Portfolio = () => {
     return (
         <React.Fragment>
             <CssBaseline />
             <Container maxWidth="md">
-                <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+                <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+                <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>Portfolio</Typography>
+                </Box>
             </Container>
         </React.Fragment>
     );
