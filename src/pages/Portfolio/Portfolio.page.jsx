f0e1375c7e62e8bdd38d650941c81080c94d3709
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-02 16:45:18 -0800

    add sections

--- src/pages/Portfolio/Portfolio.page.jsx
+++ src/pages/Portfolio/Portfolio.page.jsx
@@ -11,6 +11,31 @@ const Portfolio = () => {
             <Container maxWidth="md">
                 <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', textAlign: 'center' }}>
                     <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>Portfolio</Typography>
+                    <Container maxWidth="md">
+                        <Box sx={{ bgcolor: 'blue', height: '100vh', textAlign: 'center' }}>
+                            <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>Before Bootcamp</Typography>
+                        </Box>
+                    </Container>
+                    <Container maxWidth="md">
+                        <Box sx={{ bgcolor: 'blue', height: '100vh', textAlign: 'center' }}>
+                            <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>Bootcamp</Typography>
+                        </Box>
+                    </Container>
+                    <Container maxWidth="md">
+                        <Box sx={{ bgcolor: 'blue', height: '100vh', textAlign: 'center' }}>
+                            <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>Self-Taught</Typography>
+                        </Box>
+                    </Container>
+                    <Container maxWidth="md">
+                        <Box sx={{ bgcolor: 'blue', height: '100vh', textAlign: 'center' }}>
+                            <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>Certificates</Typography>
+                        </Box>
+                    </Container>
+                    <Container maxWidth="md">
+                        <Box sx={{ bgcolor: 'blue', height: '100vh', textAlign: 'center' }}>
+                            <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>Résumé</Typography>
+                        </Box>
+                    </Container>
                 </Box>
             </Container>
         </React.Fragment>
