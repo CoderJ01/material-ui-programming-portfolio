bb06e355dead68322f59e0d78361af1925b8f084
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-02 17:05:21 -0800

    use and modify sample Grid code from MUI

--- src/pages/Portfolio/Portfolio.page.jsx
+++ src/pages/Portfolio/Portfolio.page.jsx
@@ -4,6 +4,17 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { Typography } from '@mui/material';
 import { sections } from './Portfolio.util';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 const Portfolio = () => {
     return (
@@ -18,6 +29,16 @@ const Portfolio = () => {
                                 <Container maxWidth="md">
                                     <Box sx={{ bgcolor: 'blue', height: '100vh', textAlign: 'center' }}>
                                         <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>{section.title}</Typography>
+                                        <Box sx={{ flexGrow: 1 }}>
+                                            <Grid container spacing={4} sx={{ justifyContent: 'center', marginTop: '1%'}}>
+                                                <Grid item xs={5}>
+                                                    <Item>xs=6</Item>
+                                                </Grid>
+                                                <Grid item xs={5}>
+                                                    <Item>xs=6</Item>
+                                                </Grid>
+                                            </Grid>
+                                        </Box>
                                     </Box>
                                 </Container>
                             );
