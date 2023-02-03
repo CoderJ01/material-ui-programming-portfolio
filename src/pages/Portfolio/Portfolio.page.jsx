a6d7b789dc83f81e499fb2da9e03ce96bc45fab0
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-02 17:55:00 -0800

    clean

--- src/pages/Portfolio/Portfolio.page.jsx
+++ src/pages/Portfolio/Portfolio.page.jsx
@@ -5,16 +5,6 @@ import Container from '@mui/material/Container';
 import { Typography } from '@mui/material';
 import { sections } from './Portfolio.util';
 import Grid from '@mui/material/Grid';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
 
 const Portfolio = () => {
     return (
