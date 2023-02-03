27e74e27c09a180e396381359feb1c33af70dd43
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-02 19:29:17 -0800

    set up object to hold data for portfolio

--- src/pages/Portfolio/Portfolio.util.jsx
+++ src/pages/Portfolio/Portfolio.util.jsx
@@ -1,17 +1,266 @@
+// before bootcamp
+import c from './images/01_BeforeBootcamp/01_C-C++_Project_Pig.JPG'
+
+// bootcamp
+import horiseon from './images/02_Bootcamp/L01_HTML_horiseon.JPG'
+import portfolio from './images/02_Bootcamp/L02_Advance-CSS_first-coding-portfolio.JPG'
+import javascript from './images/02_Bootcamp/L03_JavaScript_password-generator.JPG'
+import webAPIs from './images/02_Bootcamp/L04_Web-APIs_code-quiz.JPG'
+import thirdParty from './images/02_Bootcamp/L05_Third-Party-APIs_daily-planner.JPG'
+import serverSide from './images/02_Bootcamp/L06_Server-Side-APIs_weather-dashboard.JPG'
+import project1 from './images/02_Bootcamp/L07_P1_Front-End_money-converter.JPG'
+import nodejs from './images/02_Bootcamp/L09_Node-js_README-generator.JPG'
+import object from './images/02_Bootcamp/L10_Object-Oriented-Programming_team-profile-generator.JPG'
+import express from './images/02_Bootcamp/L11_Express-js_note-taker-for-business.JPG'
+import sql from './images/02_Bootcamp/L12_SQL_employee-tracker.JPG'
+import objectRelate from './images/02_Bootcamp/L13_Object-Relational_Mapping_e-commerce-back-end.JPG'
+import mvc from './images/02_Bootcamp/L14_Model-Views-Controllers_tech-blog.JPG'
+import project2 from './images/02_Bootcamp/L15_P2_Full-Stack_InstaShoe.JPG'
+import computer from './images/02_Bootcamp/L17_Computer-Science-for-JavaScript_regex-tutorial.JPG'
+import noSQL from './images/02_Bootcamp/L18_noSQL_social-network-api.JPG'
+import reactPortfolio from './images/02_Bootcamp/L20_React_react-coding-portfolio.JPG'
+import mern from './images/02_Bootcamp/L23_P3_MERN-SPA_Nelp.JPG'
+
+// self-taught
+import python from './images/03_Self-Taught/01_Python_computer-games.JPG'
+import javaCompGames from './images/03_Self-Taught/02_Java_computer-games.JPG'
+import mean from './images/03_Self-Taught/03A_MEAN-movies-review.JPG'
+import nodeExpressMDB from './images/03_Self-Taught/04A_Node-Express-MonggDB_movies-review-backend.JPG'
+import basicJava from './images/03_Self-Taught/05_Java_order-for-McDonalds.JPG'
+import objectsJava from './images/03_Self-Taught/06_Java_man-made-object-objects.JPG'
+import diceJava from './images/03_Self-Taught/07_Java_pig-dice-game.JPG'
+import cardJava from './images/03_Self-Taught/08_Java_uno-card-game.JPG'
+import javaGUI from './images/03_Self-Taught/09_Java-GUI_find-the-gold.JPG'
+
+// certificates
+import fullstack from './images/04_Certificates/01_full-stack-web-development.JPG'
+import microsoft from './images/04_Certificates/02_Microsoft-Azure-Fundamentals.JPG'
+import objectOriented from './images/04_Certificates/03_Intro-to-OOP.JPG'
+
+// resume
+import resume from './images/05_Résumé/01_resume.JPG'
+
+
 export const sections = [
     {
-        title: "Before Bootcamp"
+        title: "Before Bootcamp",
+        content: {
+            heading1: "Pig",
+            heading2: "C++ Foundations",
+            link: "https://github.com/CoderJ01/pig",
+            image: c
+        }
     },
     {
-        title: "Bootcamp"
+        title: "Bootcamp",
+        content: [
+            {
+                heading1: "Horiseon",
+                heading2: "HTML Debug",
+                link: "https://github.com/CoderJ01/horiseon-SEO-marketing",
+                image: horiseon
+            },
+            {
+                heading1: "First Programming Portfolio",
+                heading2: "Advanced HTML/CSS",
+                link: "https://github.com/CoderJ01/coding-portfolio-website",
+                image: portfolio
+            },
+            {
+                heading1: "Password Generator",
+                heading2: "JavaScript",
+                link: "https://github.com/CoderJ01/generate-password",
+                image: javascript
+            },
+            {
+                heading1: "Code Quiz",
+                heading2: "Web APIs",
+                link: "https://github.com/CoderJ01/javascript-code-quiz",
+                image: webAPIs
+            },
+            {
+                heading1: "Work Day Planner",
+                heading2: "Third Party APIs",
+                link: "https://github.com/CoderJ01/daily-planner",
+                image: thirdParty
+            },
+            {
+                heading1: "Weather Dashboard",
+                heading2: "Server Side APIs",
+                link: "https://github.com/CoderJ01/weather-dashboard-2",
+                image: serverSide
+            },
+            {
+                heading1: "Money Converter",
+                heading2: "Front-end",
+                link: "https://github.com/CoderJ01/Project1",
+                image: project1
+            },
+            {
+                heading1: "Professional README Generator",
+                heading2: "Introduction to Node.js",
+                link: "https://github.com/CoderJ01/professional-README-generator",
+                image: nodejs
+            },
+            {
+                heading1: "Team Profile Generator",
+                heading2: "Object-Oriented Programming (OOP)",
+                link: "https://github.com/CoderJ01/team-profile-generator",
+                image: object
+            },
+            {
+                heading1: "Note Taker for Bussiness",
+                heading2: "Introduction to Express.js",
+                link: "https://github.com/CoderJ01/note-taker-for-business",
+                image: express
+            },
+            {
+                heading1: "Employee Tracker",
+                heading2: "SQL",
+                link: "https://github.com/CoderJ01/employee-tracker-for-business",
+                image: sql
+            },
+            {
+                heading1: "E-commerce Back-end",
+                heading2: "Object-Relational Mapping (ORM)",
+                link: "",
+                image: objectRelate
+            },
+            {
+                heading1: "Tech Blog",
+                heading2: "Model-View-Controller (MVC)",
+                link: "https://github.com/CoderJ01/tech-blog-2",
+                image: mvc
+            },
+            {
+                heading1: "InstaShoe",
+                heading2: "Full Stack",
+                link: "https://github.com/CoderJ01/InstaShoe",
+                image: project2
+            },
+            {
+                heading1: "Regex Tutorial",
+                heading2: "Computer Science for JavaScript",
+                link: "https://gist.github.com/CoderJ01/52a5e08cbab038ed8aa5e8fc635f16e1",
+                image: computer
+            },
+            {
+                heading1: "Social Network APIs",
+                heading2: "noSQL",
+                link: "https://github.com/CoderJ01/social-network-api/",
+                image: noSQL
+            },
+            {
+                heading1: "React Programming Portfolio",
+                heading2: "React",
+                link: "https://github.com/CoderJ01/react-programming-portfolio",
+                image: reactPortfolio
+            },
+            {
+                heading1: "Nelp",
+                heading2: "MERN/SPA",
+                link: "https://github.com/CoderJ01/review-site",
+                image: mern
+            }
+        ]
     },
     {
-        title: "Self-taught"
+        title: "Self-taught",
+        content: [
+            {
+                heading1: "Computer Games",
+                heading2: "Python Foundations",
+                link: "https://github.com/CoderJ01/python-computer-games",
+                image: python
+            },
+            {
+                heading1: "Movies Review Backend (MEAN)",
+                heading2: "Node.js, Express.js, MongoDB",
+                link: "https://github.com/CoderJ01/movies-review/tree/main/backend",
+                image: mean
+            },
+            {
+                heading1: "Computer Games",
+                heading2: "Java Foundations",
+                link: "https://github.com/CoderJ01/java-computer-games",
+                image: javaCompGames
+            },
+            {
+                heading1: "Computer Games",
+                heading2: "Java: Graphics and Events",
+                link: "https://github.com/CoderJ01/more-java-computer-games",
+                image: javaCompGames
+            },
+            {
+                heading1: "Movies Review Backend (MERN)",
+                heading2: "Node.js, Express.js, MongoD",
+                link: "https://github.com/CoderJ01/movies-review-2/tree/main/backend",
+                image: nodeExpressMDB
+            },
+            {
+                heading1: "McDonald's Order",
+                heading2: "Java: Objects",
+                link: "https://github.com/CoderJ01/order-from-McDonalds",
+                image: basicJava
+            },
+            {
+                heading1: "Man-Made Objects",
+                heading2: "Java: Abstract Classes and Interfaces",
+                link: "https://github.com/CoderJ01/man-made-objects-objects",
+                image: objectsJava
+            },
+            {
+                heading1: "Pig",
+                heading2: "Java",
+                link: "https://github.com/CoderJ01/pig-dice-game",
+                image: diceJava
+            },
+            {
+                heading1: "Uno",
+                heading2: "Java",
+                link: "https://github.com/CoderJ01/uno-card-game",
+                image: cardJava
+            },
+            {
+                heading1: "Find the Gold",
+                heading2: "Java: GUI",
+                link: "https://github.com/CoderJ01/find-the-gold",
+                image: javaGUI
+            },
+        ]
     },
     {
-        title: "Certificates"
+        title: "Certificates",
+        content: [
+            {
+                heading1: null,
+                heading2: "Full Stack Web Development",
+                link: "https://drive.google.com/file/d/1JcSJQaV1CNxyQLWpByOH89N3S9MKuuP8/view",
+                image: fullstack
+            },
+            {
+                heading1: null,
+                heading2: "Microsoft Azure Fundamentals",
+                link: "https://www.credly.com/badges/03c4183e-6f52-4a27-8022-d5477a06b13e/public_url",
+                image: microsoft
+            },
+            {
+                heading1: null,
+                heading2: "Object-Oriented Programming (Java)",
+                link: "https://www.credly.com/badges/d0eac23d-6ab9-414a-8295-e890d27257b8/public_url",
+                image: objectOriented
+            },
+        ]
     },
     {
-        title: "Résumé"
+        title: "Résumé",
+        content: [
+            {
+                heading1: null,
+                heading2: null,
+                link: "https://drive.google.com/file/d/1wnhWdnDE94cupE4AdnYXVwHoP-ZTn3ZX/view",
+                image: resume
+            },
+        ]
     },
 ];
