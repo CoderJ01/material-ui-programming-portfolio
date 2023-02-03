9e3a04119ebf596da8cf09c72be8807f115eb428
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-02 19:38:28 -0800

    import Content component into Portfolio page

--- src/pages/Portfolio/Portfolio.page.jsx
+++ src/pages/Portfolio/Portfolio.page.jsx
@@ -5,6 +5,7 @@ import Container from '@mui/material/Container';
 import { Typography } from '@mui/material';
 import { sections } from './Portfolio.data';
 import Grid from '@mui/material/Grid';
+import Content from '../../components/Content/Content.component';
 
 const Portfolio = () => {
     return (
@@ -21,30 +22,7 @@ const Portfolio = () => {
                                         <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>{section.title}</Typography>
                                         <Box sx={{ flexGrow: 1 }}>
                                             <Grid container spacing={4} sx={{ justifyContent: 'center'}}>
-                                                <Grid item xs={5}>
-                                                <Typography variant="h5" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>heading 1</Typography>
-                                                <Typography variant="h6" gutterBottom sx={{ color: 'rgb(0, 4, 55)', marginTop: '-3%', textTransform: 'uppercase' }}>heading 2</Typography>
-                                                    <Box
-                                                        sx={{
-                                                            width: '100%',
-                                                            height: 310,
-                                                            '&:hover': {
-                                                            backgroundColor: 'primary.main',
-                                                            opacity: [0.9, 0.8, 0.7],
-                                                            },
-                                                            border: '5px solid rgb(0, 4, 55)'
-                                                        }}
-                                                    >
-                                                        <a href="https://github.com/CoderJ01/Project1" target="_blank" rel="noreferrer">
-                                                            <img 
-                                                                src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=242&h=242&fit=crop&auto=format" 
-                                                                alt=""
-                                                                height="100%"
-                                                                width="100%" 
-                                                            />
-                                                        </a>
-                                                    </Box>
-                                                </Grid>
+                                                <Content/>
                                             </Grid>
                                         </Box>
                                     </Box>
