308e9105aec6e6ec8b406bbbfd7d1ec6dce69487
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-03 13:10:52 -0800

    new prop with default value of 100

--- src/components/Section/Section.component.jsx
+++ src/components/Section/Section.component.jsx
@@ -5,9 +5,9 @@ import { Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Content from '../../components/Content/Content.component';
 
-const Section = ({ dataObj = [], fromTop, sectionHeight }) => {
+const Section = ({ dataObj = [], fromTop, sectionHeight, topBoxHeight = 100 }) => {
     return (
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', textAlign: 'center', marginTop: fromTop + 'px' }}>
+        <Box sx={{ bgcolor: '#cfe8fc', height: topBoxHeight + 'vh', textAlign: 'center', marginTop: fromTop + 'px' }}>
             <Container maxWidth="md">
                 <Box sx={{ bgcolor: 'blue', minHeight: sectionHeight + 'vh', textAlign: 'center' }}>
                     <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>{dataObj.title}</Typography>
