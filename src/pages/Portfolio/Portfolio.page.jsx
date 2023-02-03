87c063a81deefa8bb0b8c6130a4fb81673d9dbd3
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-03 12:52:52 -0800

    modify height of first two Section components

--- src/pages/Portfolio/Portfolio.page.jsx
+++ src/pages/Portfolio/Portfolio.page.jsx
@@ -19,8 +19,8 @@ const Portfolio = () => {
             <Container maxWidth="md">
                 <Box sx={{ bgcolor: '#cfe8fc', height: '1000vh', textAlign: 'center' }}>
                     <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>Portfolio</Typography>
-                    <Section dataObj={beforeBootcamp} fromTop={40}/>
-                    <Section dataObj={bootcamp} fromTop={-120}/>
+                    <Section dataObj={beforeBootcamp} fromTop={40} sectionHeight={75}/>
+                    <Section dataObj={bootcamp} fromTop={-120} sectionHeight={580}/>
                     <Section dataObj={selfTaught} fromTop={3200}/>
                     {/* <Section dataObj={certificates}/> */}
                     {/* <Section dataObj={resumeObj}/> */}
