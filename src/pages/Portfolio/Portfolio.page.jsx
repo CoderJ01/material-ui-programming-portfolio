c09c7b7a463a1a8cfee88f6094227ca17351d2ce
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-02 22:19:05 -0800

    clean

--- src/pages/Portfolio/Portfolio.page.jsx
+++ src/pages/Portfolio/Portfolio.page.jsx
@@ -20,27 +20,7 @@ const Portfolio = () => {
             <Container maxWidth="md">
                 <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', textAlign: 'center' }}>
                     <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>Portfolio</Typography>
-                    {
-                        sections.map(section => {
-                            return (
-                                <Container maxWidth="md">
-                                    <Box sx={{ bgcolor: 'blue', height: '100vh', textAlign: 'center' }}>
-                                        <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>{section.title}</Typography>
-                                        <Box sx={{ flexGrow: 1 }}>
-                                            <Grid container spacing={4} sx={{ justifyContent: 'center'}}>
-                                            <Content 
-                                                heading1={section.content[0].heading1}
-                                                heading2={section.content[0].heading2}
-                                                link={section.content[0].link}
-                                                image={section.content[0].image}
-                                            />  
-                                            </Grid>
-                                        </Box>
-                                    </Box>
-                                </Container>
-                            );
-                        })
-                    }
+                    
                 </Box>
             </Container>
         </React.Fragment>
