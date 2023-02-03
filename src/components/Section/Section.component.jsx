7b92e692725c9a5f74fa224748cda8f97bbba647
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-02 22:17:21 -0800

    fill in Section component

--- src/components/Section/Section.component.jsx
+++ src/components/Section/Section.component.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import Content from '../../components/Content/Content.component';
 
-const Section = () => {
+const Section = ({ section, title }) => {
     return (
-        <div></div>
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', textAlign: 'center' }}>
+            <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>Portfolio</Typography>
+            <Container maxWidth="md">
+                <Box sx={{ bgcolor: 'blue', height: '100vh', textAlign: 'center' }}>
+                    <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>{section.title}</Typography>
+                    <Box sx={{ flexGrow: 1 }}>
+                        <Grid container spacing={4} sx={{ justifyContent: 'center'}}>
+                        <Content 
+                            heading1={section.content[0].heading1}
+                            heading2={section.content[0].heading2}
+                            link={section.content[0].link}
+                            image={section.content[0].image}
+                        />  
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </Box>
     );
 }
 
