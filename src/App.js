b8a2408952d7ccb74ddc54505b1560ddcc354a6d
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-03 13:21:30 -0800

    import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout.component";
-// import Navbar from "./components/Header/Header.component";
 import AboutMe from "./pages/AboutMe/AboutMe.page";
 import Portfolio from "./pages/Portfolio/Portfolio.page";
 import Skills from "./pages/Skills/Skills.page";
 import ContactMe from "./pages/ContactMe/ContactMe.page";
+import './App.css'
 
 function App() {
   return (
