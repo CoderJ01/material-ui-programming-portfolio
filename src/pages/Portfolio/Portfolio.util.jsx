14da04255d9cf275f2f382569e42411b7b6f6158
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-02 16:49:25 -0800

    create utils file for Portfolio

---
+++ src/pages/Portfolio/Portfolio.util.jsx
@@ -0,0 +1,17 @@
+export const sections = [
+    {
+        title: "Before Bootcamp"
+    },
+    {
+        title: "Bootcamp"
+    },
+    {
+        title: "Self-taught"
+    },
+    {
+        title: "Certificates"
+    },
+    {
+        title: "Résumé"
+    },
+];
