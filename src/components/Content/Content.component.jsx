d814a1a1b69d041f4183d75ac6e7afb4f73ea56f
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-02 20:02:32 -0800

    utilize props in Content component

--- src/components/Content/Content.component.jsx
+++ src/components/Content/Content.component.jsx
@@ -6,8 +6,8 @@ import Grid from '@mui/material/Grid';
 const Content = ({ heading1, heading2, link, image }) => {
     return (
         <Grid item xs={5}>
-        <Typography variant="h5" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>heading 1</Typography>
-        <Typography variant="h6" gutterBottom sx={{ color: 'rgb(0, 4, 55)', marginTop: '-3%', textTransform: 'uppercase' }}>heading 2</Typography>
+        <Typography variant="h5" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>{heading1}</Typography>
+        <Typography variant="h6" gutterBottom sx={{ color: 'rgb(0, 4, 55)', marginTop: '-3%', textTransform: 'uppercase' }}>{heading2}</Typography>
             <Box
                 sx={{
                     width: '100%',
@@ -19,9 +19,9 @@ const Content = ({ heading1, heading2, link, image }) => {
                     border: '5px solid rgb(0, 4, 55)'
                 }}
             >
-                <a href="https://github.com/CoderJ01/Project1" target="_blank" rel="noreferrer">
+                <a href={link} target="_blank" rel="noreferrer">
                     <img 
-                        src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=242&h=242&fit=crop&auto=format" 
+                        src={image}
                         alt=""
                         height="100%"
                         width="100%" 
