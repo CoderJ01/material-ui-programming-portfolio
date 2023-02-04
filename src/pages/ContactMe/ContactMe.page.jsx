b335e52d426e16faeb7c9623ad4c9b5ae4b295a2
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-04 15:02:00 -0800

    add heading

--- src/pages/ContactMe/ContactMe.page.jsx
+++ src/pages/ContactMe/ContactMe.page.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import { Typography } from '@mui/material';
 
 const ContactMe = () => {
     return (
         <React.Fragment>
             <CssBaseline />
             <Container maxWidth="sm">
-                <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+                <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', marginTop: '9%', textAlign: 'center' }}>
+                <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>Contact Me</Typography>
+                </Box>
             </Container>
         </React.Fragment>
     );
