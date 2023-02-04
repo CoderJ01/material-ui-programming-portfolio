8561a6da45616781047a60c2cd2145f2c74dba62
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-03 16:39:39 -0800

    add heading

--- src/pages/Skills/Skills.page.jsx
+++ src/pages/Skills/Skills.page.jsx
@@ -5,6 +5,7 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
+import { Typography } from '@mui/material';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -20,6 +21,7 @@ const Skills = () => {
             <CssBaseline />
             <Container maxWidth="md">
                 <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+                <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>Skills and Knowlegde</Typography>
                 <Box sx={{ flexGrow: 1 }}>
                     <Grid container spacing={10} sx={{ marginTop: '10px' }}>
                         <Grid xs={6}>
