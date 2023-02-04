96176dae71e217a34caa5d0ecb681436dbffab70
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-03 20:07:20 -0800

    import and use util function

--- src/components/Card/Card.component.jsx
+++ src/components/Card/Card.component.jsx
@@ -5,9 +5,10 @@ import Paper from '@mui/material/Paper';
 import { Typography } from '@mui/material';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import Box from '@mui/material/Box';
+import colorOfCheck from './Card.util';
 
 const Card = ({ dataObj = [] }) => {
-    let checkColor;
+    let checkColor = colorOfCheck(dataObj);
     
     return (
         <Grid xs={6}>
@@ -33,7 +34,7 @@ const Card = ({ dataObj = [] }) => {
                             return (
                                 <Grid item xs={6}>
                                 <Container>
-                                    <CheckCircleIcon sx={{ fill: 'green', marginTop: '4%'}}></CheckCircleIcon>
+                                    <CheckCircleIcon sx={{ fill: checkColor, marginTop: '4%'}}></CheckCircleIcon>
                                     <Container sx={{ marginTop: '-36%', marginLeft: '6%' }}>
                                         <Typography variant="h6" gutterBottom sx={{ color: 'rgb(0, 4, 55)', fontFamily: 'Monotype Corsiva'}}>{dataObj.langauge[i].name}</Typography>
                                     </Container>
