3d43651931e68aab9932773870b87b98e1a97663
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-03 17:20:36 -0800

    clean

--- src/pages/Skills/Skills.page.jsx
+++ src/pages/Skills/Skills.page.jsx
@@ -6,7 +6,6 @@ import Grid from '@mui/material/Unstable_Grid2';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import { Typography } from '@mui/material';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -40,7 +39,6 @@ const Skills = () => {
                             >
                                 <Paper elevation={3} sx={{ textAlign: 'center' }}>
                                     <Typography variant="h5" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>Heading</Typography>
-                                    <CheckCircleIcon sx={{ color: 'primary', fontSize: 'large' }}/>
                                 </Paper>
                             </Box>
                         </Grid>
