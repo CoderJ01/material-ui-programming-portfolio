0f68ddd66e97786587b23604d0880a582678eed1
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-04 14:49:35 -0800

    add Container to Contact Me page

--- src/pages/ContactMe/ContactMe.page.jsx
+++ src/pages/ContactMe/ContactMe.page.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 const ContactMe = () => {
     return (
-        <div>
-
-        </div>
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="sm">
+                <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+            </Container>
+        </React.Fragment>
     );
 }
 
