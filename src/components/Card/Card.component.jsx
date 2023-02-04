55c3f362b0717ce757844238b6fea47ed67fd8ba
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-03 19:42:35 -0800

    add <Grid xs={6}> to Card

--- src/components/Card/Card.component.jsx
+++ src/components/Card/Card.component.jsx
@@ -9,40 +9,42 @@ import Box from '@mui/material/Box';
 const Card = ({ dataObj = [] }) => {
     console.log(dataObj.langauge[0].name);
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                '& > :not(style)': {
-                m: 1,
-                width: '100%',
-                height: 410,
-                backgroundColor: 'white'
-                },
-            }}
-        >
-            <Paper elevation={3}>
-                <Container sx={{ textAlign: 'center' }}>
-                    <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)', fontFamily: 'Monotype Corsiva' }}>{dataObj.title}</Typography>
-                </Container>
-                <Grid container spacing={2} sx={{ marginTop: '-11%', marginLeft: '-5%'}}>
-                {
-                    Array.apply(0, Array(dataObj.langauge.length)).map(function(x, i) {
-                        return (
-                            <Grid item xs={6}>
-                            <Container>
-                                <CheckCircleIcon sx={{ fill: 'green', marginTop: '4%'}}></CheckCircleIcon>
-                                <Container sx={{ marginTop: '-36%', marginLeft: '6%' }}>
-                                    <Typography variant="h6" gutterBottom sx={{ color: 'rgb(0, 4, 55)', fontFamily: 'Monotype Corsiva'}}>{dataObj.langauge[i].name}</Typography>
+        <Grid xs={6}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    '& > :not(style)': {
+                    m: 1,
+                    width: '100%',
+                    height: 410,
+                    backgroundColor: 'white'
+                    },
+                }}
+            >
+                <Paper elevation={3}>
+                    <Container sx={{ textAlign: 'center' }}>
+                        <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)', fontFamily: 'Monotype Corsiva' }}>{dataObj.title}</Typography>
+                    </Container>
+                    <Grid container spacing={2} sx={{ marginTop: '-11%', marginLeft: '-5%'}}>
+                    {
+                        Array.apply(0, Array(dataObj.langauge.length)).map(function(x, i) {
+                            return (
+                                <Grid item xs={6}>
+                                <Container>
+                                    <CheckCircleIcon sx={{ fill: 'green', marginTop: '4%'}}></CheckCircleIcon>
+                                    <Container sx={{ marginTop: '-36%', marginLeft: '6%' }}>
+                                        <Typography variant="h6" gutterBottom sx={{ color: 'rgb(0, 4, 55)', fontFamily: 'Monotype Corsiva'}}>{dataObj.langauge[i].name}</Typography>
+                                    </Container>
                                 </Container>
-                            </Container>
-                        </Grid>
-                        );
-                    })
-                }
-                </Grid>
-            </Paper>
-        </Box>
+                            </Grid>
+                            );
+                        })
+                    }
+                    </Grid>
+                </Paper>
+            </Box>
+        </Grid>
     );
 }
 
