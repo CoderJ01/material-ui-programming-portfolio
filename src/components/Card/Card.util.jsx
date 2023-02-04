6bc0bea75709ba3f740b636b972cbb42c22db7e2
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-03 20:18:20 -0800

    complete replacement util function

--- src/components/Card/Card.util.jsx
+++ src/components/Card/Card.util.jsx
@@ -1,5 +1,17 @@
-const CheckColor = () => {
+const CheckColor = (proficiency) => {
+    let color;
 
+    if(proficiency === 3) {
+        color = "primary"
+    }
+    else if(proficiency === 2) {
+        color = "green"
+    }
+    else {
+        color = "yellow"
+    }
+
+    return color;
 }
 
 export default CheckColor;
