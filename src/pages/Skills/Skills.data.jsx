0336dd848516d5ea5ef18414e1b4f22cbf0965c9
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-03 18:43:35 -0800

    create data for Skills page

---
+++ src/pages/Skills/Skills.data.jsx
@@ -0,0 +1,103 @@
+export const frontend = {
+    langauge: [
+        {
+            name: "CSS",
+            proficiency: 3
+        },
+        {
+            name: "React",
+            proficiency: 2
+        },
+        {
+            name: "HTML",
+            proficiency: 2
+        },
+        {
+            name: "Responsive Design",
+            proficiency: 2
+        },
+        {
+            name: "JavaScript",
+            proficiency: 2
+        },
+        {
+            name: "jQuery",
+            proficiency: 1
+        },
+    ]
+};
+
+export const backend = {
+    langauge: [
+        {
+            name: "Java",
+            proficiency: 3
+        },
+        {
+            name: "OOP",
+            proficiency: 3
+        },
+        {
+            name: "RESTful API",
+            proficiency: 2
+        },
+        {
+            name: "Node.js",
+            proficiency: 2
+        },
+        {
+            name: "Express.js",
+            proficiency: 2
+        },
+        {
+            name: "Express.js",
+            proficiency: 2
+        },
+        {
+            name: "Mongoose (ODM)",
+            proficiency: 1
+        },
+        {
+            name: "Python",
+            proficiency: 1
+        },
+        {
+            name: "Django",
+            proficiency: 1
+        },
+    ]
+}
+
+export const database = {
+    langauge: [
+        {
+            name: "MongoDB",
+            proficiency: 2
+        },
+        {
+            name: "SQL",
+            proficiency: 1
+        },
+    ]
+};
+
+export const miscellaneous = {
+    langauge: [
+        {
+            name: "VS Code",
+            proficiency: 3
+        },
+        {
+            name: "Eclipse",
+            proficiency: 2
+        },
+        {
+            name: "Spring Suite",
+            proficiency: 1
+        },
+        {
+            name: "Microsoft Azure",
+            proficiency: 1
+        },
+    ]
+};
