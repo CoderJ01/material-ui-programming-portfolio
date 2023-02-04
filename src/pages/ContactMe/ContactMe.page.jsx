432bb7090a277cf02232a3e6f92fdc676f21af3a
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-04 15:12:06 -0800

    import MUI Card

--- src/pages/ContactMe/ContactMe.page.jsx
+++ src/pages/ContactMe/ContactMe.page.jsx
@@ -3,6 +3,10 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { Typography } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
 
 const ContactMe = () => {
     return (
@@ -11,6 +15,27 @@ const ContactMe = () => {
             <Container maxWidth="sm">
                 <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', marginTop: '9%', textAlign: 'center' }}>
                 <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>Contact Me</Typography>
+                <Card sx={{ minWidth: 275 }}>
+                    <CardContent>
+                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        Word of the Day
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                        
+                        </Typography>
+                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        adjective
+                        </Typography>
+                        <Typography variant="body2">
+                        well meaning and kindly.
+                        <br />
+                        {'"a benevolent smile"'}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
                 </Box>
             </Container>
         </React.Fragment>
