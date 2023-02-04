543b0e8ebab82c19196e64493d7a3e66032ea473
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-03 16:37:57 -0800

    set up basic Grid layout for Skills page

--- src/pages/Skills/Skills.page.jsx
+++ src/pages/Skills/Skills.page.jsx
@@ -2,13 +2,41 @@ import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Unstable_Grid2';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 const Skills = () => {
     return (
         <React.Fragment>
             <CssBaseline />
             <Container maxWidth="md">
-                <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+                <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+                <Box sx={{ flexGrow: 1 }}>
+                    <Grid container spacing={10} sx={{ marginTop: '10px' }}>
+                        <Grid xs={6}>
+                            <Item>xs=8</Item>
+                        </Grid>
+                        <Grid xs={6}>
+                            <Item>xs=4</Item>
+                        </Grid>
+                        <Grid xs={6}>
+                            <Item>xs=4</Item>
+                        </Grid>
+                        <Grid xs={6}>
+                            <Item>xs=8</Item>
+                        </Grid>
+                    </Grid>
+                </Box>
+                </Box>
             </Container>
         </React.Fragment>
     );
