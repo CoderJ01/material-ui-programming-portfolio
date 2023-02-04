fb2fd25940ba0e7b3c29afaa784181d6f5dd3a56
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-03 18:18:13 -0800

    clean

--- src/pages/Skills/Skills.page.jsx
+++ src/pages/Skills/Skills.page.jsx
@@ -3,18 +3,9 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
-import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import { Typography } from '@mui/material';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
 
 const Skills = () => {
     return (
