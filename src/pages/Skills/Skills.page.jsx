af5eb29bf328c7358cf2ec240f1d14aa6f517b66
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-03 19:50:59 -0800

    clean

--- src/pages/Skills/Skills.page.jsx
+++ src/pages/Skills/Skills.page.jsx
@@ -17,8 +17,8 @@ const Skills = () => {
         <React.Fragment>
             <CssBaseline />
             <Container maxWidth="md">
-                <Box sx={{ bgcolor: '#cfe8fc', height: '185vh' }}>
-                    <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)', textAlign: 'center', marginTop: '6%', backgroundColor: 'white'  }}>Skills and Knowlegde</Typography>
+                <Box sx={{ height: '185vh', backgroundColor: 'white' }}>
+                    <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)', textAlign: 'center', marginTop: '6%'}}>Skills and Knowlegde</Typography>
                     <Box sx={{ flexGrow: 1 }}>
                         <Grid container spacing={10} sx={{ marginTop: '10px' }}>
                             <Card dataObj={frontend}/>
