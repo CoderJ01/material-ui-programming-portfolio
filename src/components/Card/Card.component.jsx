a58f8fc3af29fc5fe366d7316e1304b9df1a3e30
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-03 19:00:54 -0800

    fill in Card component

--- src/components/Card/Card.component.jsx
+++ src/components/Card/Card.component.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Unstable_Grid2';
+import Paper from '@mui/material/Paper';
+import { Typography } from '@mui/material';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 const Card = () => {
     return (
-        <div>
+        <Paper elevation={3}>
+            <Container sx={{ textAlign: 'center' }}>
+                <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)', fontFamily: 'Monotype Corsiva' }}>Heading</Typography>
+            </Container>
             
-        </div>
+            <Grid container spacing={2} sx={{ marginTop: '-11%', marginLeft: '-5%'}}>
+                <Grid item xs={6}>
+                    <Container>
+                        <CheckCircleIcon sx={{ fill: 'green', marginTop: '4%'}}></CheckCircleIcon>
+                        <Container sx={{ marginTop: '-36%', marginLeft: '6%' }}>
+                            <Typography variant="h6" gutterBottom sx={{ color: 'rgb(0, 4, 55)', fontFamily: 'Monotype Corsiva'}}>Language</Typography>
+                        </Container>
+                    </Container>
+                </Grid>
+            </Grid>
+        </Paper>
     );
 }
 
