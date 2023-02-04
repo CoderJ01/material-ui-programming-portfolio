c49ef3217b56f201151a414ba0f7c87b9a2dd386
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-03 16:24:37 -0800

    add Container to Skills page

--- src/pages/Skills/Skills.page.jsx
+++ src/pages/Skills/Skills.page.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 const Skills = () => {
     return (
-        <div>
-
-        </div>
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="md">
+                <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+            </Container>
+        </React.Fragment>
     );
 }
 
