5b9f6720ea563d17f941ec0d007f9aaea56ddd1d
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-04 17:30:04 -0800

    email validation

--- src/pages/ContactMe/ContactMe.page.jsx
+++ src/pages/ContactMe/ContactMe.page.jsx
@@ -29,6 +29,11 @@ const ContactMe = () => {
         }
     }
 
+    const isValidEmail = email => {
+        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(String(email).toLowerCase());
+    };
+
     return (
         <React.Fragment>
             <CssBaseline />
