bb6822b52a8819863cb122c25f8e208983cd65d9
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-04 20:37:27 -0800

    clean

--- src/components/InputSection/InputSection.component.jsx
+++ src/components/InputSection/InputSection.component.jsx
@@ -1,57 +1,11 @@
 import React from 'react';
-import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { Typography } from '@mui/material';
 import CardContent from '@mui/material/CardContent';
-import TextField from '@mui/material/TextField';
 import { SelectInputSectionType } from './InputSection.utils';
 
 const InputSection = ({ fromTop = 0, label, placeholder, value, setState, multiLine = false }) => {
     let formBoxSection = SelectInputSectionType(placeholder, value, setState, multiLine);
-    // if(multiLine === true) {
-    //     formBoxSection = 
-    //     <Box
-    //         component="form"
-    //         sx={{
-    //             '& .MuiTextField-root': { m: 1, width: '96%', marginTop: '-1%' },
-    //         }}
-    //         noValidate
-    //         autoComplete="off"
-    //         >
-    //             <TextField
-    //             id="filled-multiline-static"
-    //             multiline
-    //             rows={4}
-    //             variant="outlined"
-    //             placeholder={placeholder}
-    //             value={value}
-    //             onChange={e => {
-    //                 setState(e.target.value)
-    //             }}
-    //             />
-    //     </Box>
-    // }
-    // else {
-    //     formBoxSection = 
-    //     <Box
-    //         component="form"
-    //         sx={{
-    //             '& > :not(style)': { m: 1, width: '96%', marginTop: '-1%'}, 
-    //         }}
-    //         noValidate
-    //         autoComplete="off"
-    //         >
-    //         <TextField 
-    //             id="outlined-basic" 
-    //             variant="outlined" 
-    //             placeholder={placeholder}
-    //             value={value}
-    //             onChange={e => {
-    //                 setState(e.target.value)
-    //             }}
-    //         />
-    //     </Box>
-    // }
     return (
         <CardContent sx={{ textAlign: 'left', marginTop: fromTop }}>
             <Container sx={{ marginLeft: '-3%'}}>
