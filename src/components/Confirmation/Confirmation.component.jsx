9e2779b9df261b8dd61cd0ef26e4e58d9a2170ef
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-05 15:06:59 -0800

    Confirmation component do-over with props

--- src/components/Confirmation/Confirmation.component.jsx
+++ src/components/Confirmation/Confirmation.component.jsx
@@ -1,9 +1,36 @@
 import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
-export default function Confirmation() {
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '3px solid rgb(0, 4, 55)',
+    boxShadow: 24,
+    p: 4,
+};
+
+export default function Confirmation({ pop, unpop }) {
   return (
-    <div>
-      
-    </div>
+    <Modal
+        open={pop}
+        onClose={unpop}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+    >
+        <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2" sx={{color: 'rgb(0, 4, 55)', textAlign: 'center', fontWeight: 575}}>
+                Confirmation
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2, color: 'rgb(0, 4, 55)', marginTop: '0'}}>
+                Your message will be received shortly. Wait for up to 48 hours for a response. Have a nice day!
+            </Typography>
+        </Box>
+    </Modal>
   );
 }
