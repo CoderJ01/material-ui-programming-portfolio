2ba388432a97390ce40c8df79f9054fdd0024484
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-04 20:24:48 -0800

    conditionally render type of input section - single or multi

--- src/components/InputSection/InputSection.component.jsx
+++ src/components/InputSection/InputSection.component.jsx
@@ -6,29 +6,57 @@ import CardContent from '@mui/material/CardContent';
 import TextField from '@mui/material/TextField';
 
 const InputSection = ({ fromTop = 0, label, placeholder, value, setState, multiLine = false }) => {
+    let formBoxSection;
+    if(multiLine === true) {
+        formBoxSection = 
+        <Box
+            component="form"
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '96%', marginTop: '-1%' },
+            }}
+            noValidate
+            autoComplete="off"
+            >
+                <TextField
+                id="filled-multiline-static"
+                multiline
+                rows={4}
+                variant="outlined"
+                placeholder={placeholder}
+                value={value}
+                onChange={e => {
+                    setState(e.target.value)
+                }}
+                />
+        </Box>
+    }
+    else {
+        formBoxSection = 
+        <Box
+            component="form"
+            sx={{
+                '& > :not(style)': { m: 1, width: '96%', marginTop: '-1%'}, 
+            }}
+            noValidate
+            autoComplete="off"
+            >
+            <TextField 
+                id="outlined-basic" 
+                variant="outlined" 
+                placeholder={placeholder}
+                value={value}
+                onChange={e => {
+                    setState(e.target.value)
+                }}
+            />
+        </Box>
+    }
     return (
         <CardContent sx={{ textAlign: 'left', marginTop: fromTop }}>
             <Container sx={{ marginLeft: '-3%'}}>
                 <Typography variant="h6" gutterBottom sx={{ color: 'rgb(0, 4, 55)'}}>{label}</Typography>
+                {formBoxSection}
             </Container>
-            <Box
-                component="form"
-                sx={{
-                    '& > :not(style)': { m: 1, width: '96%', marginTop: '-1%'}, 
-                }}
-                noValidate
-                autoComplete="off"
-                >
-                <TextField 
-                    id="outlined-basic" 
-                    variant="outlined" 
-                    placeholder={placeholder}
-                    value={value}
-                    onChange={e => {
-                        setState(e.target.value)
-                    }}
-                />
-            </Box>
         </CardContent>
     );
 }
