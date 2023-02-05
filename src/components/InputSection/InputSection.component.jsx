7bad9d32fadc2262dbbf98a2b4536b10d841d88f
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-04 20:55:14 -0800

    change default value of fromTop

--- src/components/InputSection/InputSection.component.jsx
+++ src/components/InputSection/InputSection.component.jsx
@@ -4,10 +4,10 @@ import { Typography } from '@mui/material';
 import CardContent from '@mui/material/CardContent';
 import { SelectInputSectionType } from './InputSection.utils';
 
-const InputSection = ({ fromTop = 0, label, placeholder, value, setState, multiLine = false }) => {
+const InputSection = ({ fromTop = -5, label, placeholder, value, setState, multiLine = false }) => {
     let formBoxSection = SelectInputSectionType(placeholder, value, setState, multiLine);
     return (
-        <CardContent sx={{ textAlign: 'left', marginTop: fromTop }}>
+        <CardContent sx={{ textAlign: 'left', marginTop: fromTop + '%' }}>
             <Container sx={{ marginLeft: '-3%'}}>
                 <Typography variant="h6" gutterBottom sx={{ color: 'rgb(0, 4, 55)'}}>{label}</Typography>
             </Container>
