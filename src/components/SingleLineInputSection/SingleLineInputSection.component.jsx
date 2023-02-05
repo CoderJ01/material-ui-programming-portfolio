101ac785c734af95bb6b917c294f7ccb070de971
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-04 19:46:47 -0800

    fill in new component

--- src/components/SingleLineInputSection/SingleLineInputSection.component.jsx
+++ src/components/SingleLineInputSection/SingleLineInputSection.component.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { Typography } from '@mui/material';
+import CardContent from '@mui/material/CardContent';
+import TextField from '@mui/material/TextField';
 
 const SingleLineInputSection = () => {
+    const [name, setName] = useState('');
     return (
-        <div>
-
-        </div>
+        <CardContent sx={{ textAlign: 'left' }}>
+            <Container sx={{ marginLeft: '-3%'}}>
+                <Typography variant="h6" gutterBottom sx={{ color: 'rgb(0, 4, 55)'}}>Name</Typography>
+            </Container>
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '96%', marginTop: '-1%'}, 
+                }}
+                noValidate
+                autoComplete="off"
+                >
+                <TextField 
+                    id="outlined-basic" 
+                    variant="outlined" 
+                    placeholder="Name"
+                    value={name}
+                    onChange={e => {
+                        setName(e.target.value)
+                    }}
+                />
+            </Box>
+        </CardContent>
     );
 }
 
