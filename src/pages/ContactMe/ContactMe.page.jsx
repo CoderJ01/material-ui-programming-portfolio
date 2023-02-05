bc5a150fd7a8a0ff4170629e93002f10455a937f
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-04 19:29:44 -0800

    import and use Form component

--- src/pages/ContactMe/ContactMe.page.jsx
+++ src/pages/ContactMe/ContactMe.page.jsx
@@ -3,6 +3,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { Typography } from '@mui/material';
+import Form from '../../components/Form/Form.component';
 
 const ContactMe = () => {
     return (
@@ -11,7 +12,7 @@ const ContactMe = () => {
             <Container maxWidth="sm">
                 <Box sx={{ height: '100vh', marginTop: '9%', textAlign: 'center' }}>
                 <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>Contact Me</Typography>
-                
+                    <Form />
                 </Box>
             </Container>
         </React.Fragment>
