c30d031f195cbccac3567bd7619d8aa130387eae
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-05 14:09:34 -0800

    style modal

--- src/components/PortfolioButton/PortfolioButton.component.jsx
+++ src/components/PortfolioButton/PortfolioButton.component.jsx
@@ -12,7 +12,7 @@ const style = {
     transform: 'translate(-50%, -50%)',
     width: 400,
     bgcolor: 'background.paper',
-    border: '2px solid #000',
+    border: '3px solid rgb(0, 4, 55)',
     boxShadow: 24,
     p: 4,
   };
@@ -37,7 +37,8 @@ const PortfolioButton = ({ event, label }) => {
                     } 
                 }}
                 onClick={handleOpen}
-            >{label}
+            >
+                {label}
             </Button>
             <Modal
                 open={open}
@@ -46,11 +47,11 @@ const PortfolioButton = ({ event, label }) => {
                 aria-describedby="modal-modal-description"
             >
                 <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        Text in a modal
+                    <Typography id="modal-modal-title" variant="h6" component="h2" sx={{color: 'rgb(0, 4, 55)', textAlign: 'center', fontWeight: 575}}>
+                        Confirmation
                     </Typography>
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                    <Typography id="modal-modal-description" sx={{ mt: 2, color: 'rgb(0, 4, 55)', marginTop: '0'}}>
+                        Your message will be received shortly! Wait for up to 48 hours for a response. Have a nice day;
                     </Typography>
                 </Box>
             </Modal>
