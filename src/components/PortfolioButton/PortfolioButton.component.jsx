1324f8dddc8d1a9c32b1740e4892df23b033a1b9
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-04 20:49:44 -0800

    fill in PortfolioButton component

--- src/components/PortfolioButton/PortfolioButton.component.jsx
+++ src/components/PortfolioButton/PortfolioButton.component.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
-const PortfolioButton = () => {
+const PortfolioButton = ({ event, label }) => {
     return (
-        <div>
-            
-        </div>
+        <Button 
+            variant="contained" 
+            size="small" 
+            sx={{ 
+                backgroundColor: 'rgb(0, 4, 55)', 
+                color: 'rgb(255, 253, 190)',
+                ':hover': { 
+                    backgroundColor: 'rgb(255, 253, 170)',
+                    border: '2px solid rgb(0, 4, 55)',
+                    color: 'rgb(0, 4, 55)',
+                    fontWeight: 650
+                } 
+            }}
+            onClick={event}
+        >{label}</Button>
     );
 }
 
