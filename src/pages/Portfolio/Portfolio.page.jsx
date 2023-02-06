11331e79f4c56bc41a277842cff082fae8647570
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-05 21:12:31 -0800

    clean

--- src/pages/Portfolio/Portfolio.page.jsx
+++ src/pages/Portfolio/Portfolio.page.jsx
@@ -19,11 +19,11 @@ const Portfolio = () => {
             <Container maxWidth="md">
                 <Box sx={{ height: '1225vh', textAlign: 'center', marginTop: '6%', backgroundColor: 'rgb(255, 253, 208)' }}>
                     <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>Portfolio</Typography>
-                    <Section dataObj={beforeBootcamp} fromTop={40} sectionHeight={75}/>
-                    <Section dataObj={bootcamp} fromTop={-120} sectionHeight={580}/>
-                    <Section dataObj={selfTaught} fromTop={3200} sectionHeight={325}/>
-                    <Section dataObj={certificates} fromTop={1525} sectionHeight={137}/>
-                    <Section dataObj={resumeObj} fromTop={290} sectionHeight={63} topBoxHeight={63}/>
+                    <Section dataObj={beforeBootcamp} sectionHeight={75}/>
+                    <Section dataObj={bootcamp} sectionHeight={580}/>
+                    <Section dataObj={selfTaught} sectionHeight={325}/>
+                    <Section dataObj={certificates} sectionHeight={137}/>
+                    <Section dataObj={resumeObj} sectionHeight={63} topBoxHeight={63}/>
                 </Box>
             </Container>
         </React.Fragment>
