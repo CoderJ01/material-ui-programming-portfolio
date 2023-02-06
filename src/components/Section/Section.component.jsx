288d315fab2f945419921d5677494c9f5bff1337
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-05 21:13:18 -0800

    clean

--- src/components/Section/Section.component.jsx
+++ src/components/Section/Section.component.jsx
@@ -7,29 +7,27 @@ import Content from '../../components/Content/Content.component';
 
 const Section = ({ dataObj = [], fromTop, sectionHeight, topBoxHeight = 100 }) => {
     return (
-        <Box sx={{ height: topBoxHeight + 'vh', textAlign: 'center', marginTop: fromTop + 'px' }}>
-            <Container maxWidth="md">
-                <Box sx={{ minHeight: sectionHeight + 'vh', textAlign: 'center' }}>
-                    <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>{dataObj.title}</Typography>
-                    <Box sx={{ flexGrow: 1 }}>
-                        <Grid container spacing={4} sx={{ justifyContent: 'center'}}>
-                        {
-                            Array.apply(0, Array(dataObj.content.length)).map(function(x, i) {
-                                return (
-                                    <Content
-                                        heading1={dataObj.content[i].heading1}
-                                        heading2={dataObj.content[i].heading2}
-                                        link={dataObj.content[i].link}
-                                        image={dataObj.content[i].image}
-                                    />
-                                );
-                            })
-                        }
-                        </Grid>
-                    </Box>
+        <Container maxWidth="md">
+            <Box sx={{ minHeight: sectionHeight + 'vh', textAlign: 'center' }}>
+                <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>{dataObj.title}</Typography>
+                <Box sx={{ flexGrow: 1 }}>
+                    <Grid container spacing={4} sx={{ justifyContent: 'center'}}>
+                    {
+                        Array.apply(0, Array(dataObj.content.length)).map(function(x, i) {
+                            return (
+                                <Content
+                                    heading1={dataObj.content[i].heading1}
+                                    heading2={dataObj.content[i].heading2}
+                                    link={dataObj.content[i].link}
+                                    image={dataObj.content[i].image}
+                                />
+                            );
+                        })
+                    }
+                    </Grid>
                 </Box>
-            </Container>
-        </Box>
+            </Box>
+        </Container>
     );
 }
 
