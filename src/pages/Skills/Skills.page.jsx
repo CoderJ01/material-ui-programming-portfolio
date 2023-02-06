98eba98538a7bfd352b5c70ecffe49df5f067e50
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-05 17:07:45 -0800

    import

--- src/pages/Skills/Skills.page.jsx
+++ src/pages/Skills/Skills.page.jsx
@@ -13,6 +13,7 @@ import {
     description } 
 from './Skills.data';
 import Paper from '@mui/material/Paper';
+import './Skills.style.css'
 
 const Skills = () => {
     return (
@@ -20,27 +21,28 @@ const Skills = () => {
             <CssBaseline />
             <Container maxWidth="md">
                 <Box sx={{ height: '220vh', backgroundColor: 'rgb(255, 253, 208)' }}>
-                    <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)', textAlign: 'center', marginTop: '6%'}}>Skills and Knowledge</Typography>
-                    <Container>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            flexWrap: 'wrap',
-                            '& > :not(style)': {
-                            m: 1,
-                            width: '100%',
-                            height: 200,
-                            },
-                        }}
-                        >
-                        <Paper elevation={8} sx={{ textAlign: 'center'}}>
-                            <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)', fontFamily: 'Monotype Corsiva' }}>Description</Typography>
-                            <Typography variant="h6" gutterBottom sx={{ color: 'rgb(0, 4, 55)', fontFamily: 'Monotype Corsiva' }}>{description}</Typography>
-                        </Paper>
-                    </Box>
+                    <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)', textAlign: 'center', marginTop: '6%'}} className='skills-heading'>Skills and Knowledge</Typography>
+                    <Container className='skills-container'>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexWrap: 'wrap',
+                                '& > :not(style)': {
+                                m: 1,
+                                width: '100%',
+                                height: 200,
+                                },
+                            }}
+                            className='skills-box'
+                            >
+                            <Paper elevation={8} sx={{ textAlign: 'center'}}  className='skills-paper'>
+                                <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)', fontFamily: 'Monotype Corsiva' }} className='skills-description-heading'>Description</Typography>
+                                <Typography variant="h6" gutterBottom sx={{ color: 'rgb(0, 4, 55)', fontFamily: 'Monotype Corsiva' }} className='skills-description'>{description}</Typography>
+                            </Paper>
+                        </Box>
                     </Container>
                     <Box sx={{ flexGrow: 1 }}>
-                        <Grid container spacing={10} sx={{ marginTop: '10px' }}>
+                        <Grid container spacing={10} sx={{ marginTop: '10px' }} className="skills-grid">
                             <PortfolioCard dataObj={frontend}/>
                             <PortfolioCard dataObj={backend}/>
                             <PortfolioCard dataObj={database}/>
