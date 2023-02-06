091b1133a2e2e0bda85a61348676afab8a51300a
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-05 16:28:13 -0800

    import

--- src/pages/AboutMe/AboutMe.page.jsx
+++ src/pages/AboutMe/AboutMe.page.jsx
@@ -6,19 +6,21 @@ import Avatar from '@mui/material/Avatar';
 import image from './images/01_avatar.png'
 import { Typography } from '@mui/material';
 import { paragraphs } from './AboutMe.data';
+import './AboutMe.style.css'
 
 const AboutMe = () => {
     return (
         <React.Fragment>
             <CssBaseline />
             <Container maxWidth="md">
-                <Box sx={{ height: '140vh', textAlign: 'center', marginTop: '6%', backgroundColor: 'rgb(255, 253, 208)'}}>
-                <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>About Me</Typography>
+                <Box sx={{ height: '140vh', textAlign: 'center', marginTop: '6%', backgroundColor: 'rgb(255, 253, 208)'}}  className='about-me-box'>
+                <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }} className='about-me-heading'>About Me</Typography>
                 <div style={{ display: 'flex', justifyContent:'center'}}>
                     <Avatar
                         alt="Remy Sharp"
                         src={image}
                         sx={{ width: 150, height: 150, border: '5px solid rgb(0, 4, 55)' }}
+                        className='about-me-avatar'
                     />
                 </div>
                 <Container maxWidth="sm" sx={{marginTop: '5%'}}>
@@ -26,7 +28,7 @@ const AboutMe = () => {
                         {
                             paragraphs.map(paragraph => {
                                 return (
-                                    <Typography variant="body1" gutterBottom sx={{ color: 'black' }}>{paragraph}</Typography>
+                                    <Typography variant="body1" gutterBottom sx={{ color: 'black' }} className='about-me-body'>{paragraph}</Typography>
                                 );
                             })
                         }
