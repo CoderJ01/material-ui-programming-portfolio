1473a94c291a4ba6b4d6386bcbb7184b7153df6c
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-07 15:34:22 -0800

    use MUI breakpoints for Content component

--- src/components/Content/Content.component.jsx
+++ src/components/Content/Content.component.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import { Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
-import './Content.style.css';
+// import './Content.style.css';
 
 const Content = ({ heading1, heading2, link, image }) => {
     return (
-        <Grid item xs={5} className="content-grid">
+        <Grid item xs={12} sm={7} md={8} lg={8} xl={6} className="content-grid">
             <div className="content-heading-group">
                 <Typography variant="h5" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }} className="content-heading-1">{heading1}</Typography>
                 <Typography variant="h6" gutterBottom sx={{ color: 'rgb(0, 4, 55)', marginTop: '-3%', textTransform: 'uppercase' }} className="content-heading-2">{heading2}</Typography>
