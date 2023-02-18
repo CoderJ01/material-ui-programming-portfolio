3e05813b32be348dd1889831c5eb3e18a597ddde
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-17 20:05:33 -0800

    import needed classes and constants from graphql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,3 +4,12 @@ const Language = require('../models/Language');
 const Section = require('../models/Section');
 const Content = require('../models/Content');
 
+const { 
+    GraphQLObjectType,  
+    GraphQLSchema, 
+    GraphQLList,
+    GraphQLNonNull,
+    GraphQLID, 
+    GraphQLString,
+    GraphQLInt
+} = require('graphql');
