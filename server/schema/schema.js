86e4f080f3f5d6624cfffd22e6fc2b2aab840d61
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-17 20:01:52 -0800

    create schema for grapql

---
+++ server/schema/schema.js
@@ -0,0 +1,6 @@
+// Mongoose models
+const SkillCategory = require('../models/SkillCategory');
+const Language = require('../models/Language');
+const Section = require('../models/Section');
+const Content = require('../models/Content');
+
