c945a7dee2eb9df2fa8b606e96af7b4d042ce574
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-19 17:18:41 -0800

    implement ApolloProvider in React client

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,30 @@ import AboutMe from "./pages/AboutMe/AboutMe.page";
 import Portfolio from "./pages/Portfolio/Portfolio.page";
 import Skills from "./pages/Skills/Skills.page";
 import ContactMe from "./pages/ContactMe/ContactMe.page";
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import './App.css'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  InMemoryCache
+});
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<AboutMe/>}/>
-          <Route path="portfolio" element={<Portfolio/>}/>
-          <Route path="skills" element={<Skills/>}/>
-          <Route path="contact-me" element={<ContactMe/>}/>
-          <Route path="*" element={<AboutMe/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout/>}>
+              <Route index element={<AboutMe/>}/>
+              <Route path="portfolio" element={<Portfolio/>}/>
+              <Route path="skills" element={<Skills/>}/>
+              <Route path="contact-me" element={<ContactMe/>}/>
+              <Route path="*" element={<AboutMe/>}/>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ApolloProvider>
     </div>
   );
 }
