c4044673e34760211d5d8ac8f778c55a58298cc8
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-19 21:59:03 -0800

    render paragraphs in introduction with query

--- client/src/pages/AboutMe/AboutMe.page.jsx
+++ client/src/pages/AboutMe/AboutMe.page.jsx
@@ -6,35 +6,47 @@ import Avatar from '@mui/material/Avatar';
 import image from './images/01_avatar.png'
 import { Typography } from '@mui/material';
 import { paragraphs } from './AboutMe.data';
+import { GET_ALL_PARAGRAPHS } from '../../graphql/queries/Paragraph.query';
+import { useQuery } from '@apollo/client';
 
 const AboutMe = () => {
+
+    const { loading, error, data } = useQuery(GET_ALL_PARAGRAPHS);
+
     return (
-        <React.Fragment>
-            <CssBaseline />
-            <Container maxWidth="md">
-                <Box sx={{ minHeight: '155vh', textAlign: 'center', marginTop: '6%', backgroundColor: 'rgb(255, 253, 208)'}}>
-                <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>About Me</Typography>
-                <div style={{ display: 'flex', justifyContent:'center'}}>
-                    <Avatar
-                        alt="Remy Sharp"
-                        src={image}
-                        sx={{ width: 150, height: 150, border: '5px solid rgb(0, 4, 55)' }}
-                    />
-                </div>
-                <Container maxWidth="sm" sx={{marginTop: '5%', border: '2px solid rgb(0, 4, 55)', background: 'white', borderRadius: '10px' }}>
-                    <Box sx={{ minHeight: '100vh' }}>
-                        {
-                            paragraphs.map((paragraph, i) => {
-                                return (
-                                    <Typography variant="body1" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }} key={i}>{paragraph}</Typography>
-                                );
-                            })
-                        }
-                    </Box>
-                </Container>
-                </Box>
-            </Container>
-        </React.Fragment>
+        <>
+        {
+            !loading && !error && 
+            (
+                <React.Fragment>
+                    <CssBaseline />
+                    <Container maxWidth="md">
+                        <Box sx={{ minHeight: '155vh', textAlign: 'center', marginTop: '6%', backgroundColor: 'rgb(255, 253, 208)'}}>
+                        <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>About Me</Typography>
+                        <div style={{ display: 'flex', justifyContent:'center'}}>
+                            <Avatar
+                                alt="Remy Sharp"
+                                src={image}
+                                sx={{ width: 150, height: 150, border: '5px solid rgb(0, 4, 55)' }}
+                            />
+                        </div>
+                        <Container maxWidth="sm" sx={{marginTop: '5%', border: '2px solid rgb(0, 4, 55)', background: 'white', borderRadius: '10px' }}>
+                            <Box sx={{ minHeight: '100vh' }}>
+                                {
+                                    Array.apply(0, Array(data.paragraphs.length)).map(function(x, i) {
+                                        return (
+                                            <Typography variant="body1" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }} key={i}>{data.paragraphs[i].text}</Typography>
+                                        );
+                                    })
+                                }
+                            </Box>
+                        </Container>
+                        </Box>
+                    </Container>
+                </React.Fragment>
+            )
+        }
+        </>
     );
 }
 
