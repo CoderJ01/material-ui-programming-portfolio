53d91c616edb1f8482abd65db7b78c2c8b1e7887
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-19 21:40:35 -0800

    change name

--- server/models/IntroParagraph.js
+++ server/models/IntroParagraph.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const IntroParagraphSchema = new mongoose.Schema({
+const ParagraphSchema = new mongoose.Schema({
     text: {
         type: String,
         required: true
     }
 });
 
-module.exports = mongoose.model('IntroParagraph', IntroParagraphSchema);
+module.exports = mongoose.model('Paragraph', ParagraphSchema);
