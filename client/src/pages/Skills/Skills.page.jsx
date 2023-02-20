87b97311777781d88f144d684bf250acf5059bf5
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-19 18:51:40 -0800

    indent

--- client/src/pages/Skills/Skills.page.jsx
+++ client/src/pages/Skills/Skills.page.jsx
@@ -20,42 +20,42 @@ const Skills = () => {
             !loading && !error && 
             (
                 <React.Fragment>
-                <CssBaseline />
-                <Container maxWidth="md">
-                    <Box sx={{ minHeight: '220vh', backgroundColor: 'rgb(255, 253, 208)' }}>
-                        <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)', textAlign: 'center', marginTop: '6%'}}>Skills and Knowledge</Typography>
-                        <Container>
-                            <Box
-                                sx={{
-                                    display: 'flex',
-                                    flexWrap: 'wrap',
-                                    '& > :not(style)': {
-                                    m: 1,
-                                    width: '100%',
-                                    minHeight: 200,
-                                    },
-                                }}
-                                >
-                                <Paper elevation={8} sx={{ textAlign: 'center'}}>
-                                    <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)', fontFamily: 'Monotype Corsiva' }}>Description</Typography>
-                                    <Typography variant="h6" gutterBottom sx={{ color: 'rgb(0, 4, 55)', fontFamily: 'Monotype Corsiva' }}>{description}</Typography>
-                                </Paper>
+                    <CssBaseline />
+                    <Container maxWidth="md">
+                        <Box sx={{ minHeight: '220vh', backgroundColor: 'rgb(255, 253, 208)' }}>
+                            <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)', textAlign: 'center', marginTop: '6%'}}>Skills and Knowledge</Typography>
+                            <Container>
+                                <Box
+                                    sx={{
+                                        display: 'flex',
+                                        flexWrap: 'wrap',
+                                        '& > :not(style)': {
+                                        m: 1,
+                                        width: '100%',
+                                        minHeight: 200,
+                                        },
+                                    }}
+                                    >
+                                    <Paper elevation={8} sx={{ textAlign: 'center'}}>
+                                        <Typography variant="h4" gutterBottom sx={{ color: 'rgb(0, 4, 55)', fontFamily: 'Monotype Corsiva' }}>Description</Typography>
+                                        <Typography variant="h6" gutterBottom sx={{ color: 'rgb(0, 4, 55)', fontFamily: 'Monotype Corsiva' }}>{description}</Typography>
+                                    </Paper>
+                                </Box>
+                            </Container>
+                            <Box sx={{ flexGrow: 1 }}>
+                                <Grid container spacing={4} sx={{ marginTop: '10px', justifyContent: 'center' }}>
+                                    {
+                                        Array.apply(0, Array(data.skillCategories.length)).map(function(x, i) {
+                                            return (
+                                                <PortfolioCard dataObj={data.skillCategories[i]} key={i}/>
+                                            );
+                                        })
+                                    }
+                                </Grid>
                             </Box>
-                        </Container>
-                        <Box sx={{ flexGrow: 1 }}>
-                            <Grid container spacing={4} sx={{ marginTop: '10px', justifyContent: 'center' }}>
-                                {
-                                    Array.apply(0, Array(data.skillCategories.length)).map(function(x, i) {
-                                        return (
-                                            <PortfolioCard dataObj={data.skillCategories[i]} key={i}/>
-                                        );
-                                    })
-                                }
-                            </Grid>
                         </Box>
-                    </Box>
-                </Container>
-            </React.Fragment>
+                    </Container>
+                </React.Fragment>
             )
         }
         </>
