d304f2f289f0352f239dbbac520901cdb601294c
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-19 18:06:32 -0800

    render content using query

--- client/src/pages/Portfolio/Portfolio.page.jsx
+++ client/src/pages/Portfolio/Portfolio.page.jsx
@@ -5,14 +5,19 @@ import Container from '@mui/material/Container';
 import { Typography } from '@mui/material';
 import { 
     beforeBootcamp, 
-    bootcamp, 
-    selfTaught, 
-    certificates, 
-    resume 
+    // bootcamp, 
+    // selfTaught, 
+    // certificates, 
+    // resume 
 } from './Portfolio.data';
 import Section from '../../components/Section/Section.component';
+import { GET_ALL_SECTIONS_WITH_DETAILS } from '../../graphql/queries/Section.query';
+import { useQuery } from '@apollo/client';
 
 const Portfolio = () => {
+    
+    const { loading, error, data } = useQuery(GET_ALL_SECTIONS_WITH_DETAILS);
+    
     return (
         <React.Fragment>
             <CssBaseline />
@@ -20,11 +25,20 @@ const Portfolio = () => {
                 <Box sx={{ minHeight: '50vh', textAlign: 'center', marginTop: '6%', backgroundColor: 'rgb(255, 253, 208)' }}>
                     <Typography variant="h3" gutterBottom sx={{ color: 'rgb(0, 4, 55)' }}>Portfolio</Typography>
                     <Container sx={{ marginTop: '6%' }}>
-                        <Section dataObj={beforeBootcamp} fromTop={0}/>
-                        <Section dataObj={bootcamp}/>
+                        <Section dataObj={data.sections[0]} fromTop={0}/>
+                        {
+                              Array.apply(0, Array(data.sections.length)).map(function(x, i) {
+                                if(i !== 0) {
+                                    return (
+                                        <Section dataObj={data.sections[i]}/>
+                                    )
+                                }
+                              })
+                        }
+                        {/* <Section dataObj={bootcamp}/>
                         <Section dataObj={selfTaught}/>
                         <Section dataObj={certificates}/>
-                        <Section dataObj={resume}/>
+                        <Section dataObj={resume}/> */}
                     </Container>
                 </Box>
             </Container>
