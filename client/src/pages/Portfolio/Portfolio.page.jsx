d3b21734bb63e3b7fb97088e34392bbaa6529cd8
CoderJ01 <buildingguy@sbcglobal.net>
2023-06-08 14:58:08 -0700

    clean

--- client/src/pages/Portfolio/Portfolio.page.jsx
+++ client/src/pages/Portfolio/Portfolio.page.jsx
@@ -32,7 +32,7 @@ const Portfolio = () => {
                                 <Section dataObj={data.sections[0]} fromTop={0} key={0}/>
                                 {
                                     Array.apply(0, Array(data.sections.length)).map(function(x, i) {
-                                        if(i !== 0) {
+                                        if(i !== 0 && data.sections[i].title !== 'Résumé') {
                                             return (
                                                 <Section dataObj={data.sections[i]} key={i}/>
                                             )
